c1c9324179955d0f987aec2d3f46af101fda4bdf
SHAN1461 <163760646+SHAN1461@users.noreply.github.com>
2024-04-23 13:11:32 +0530

    Add files via upload

---
+++ Project/Bike.java
@@ -0,0 +1,45 @@
+package Project;
+
+public class Bike {
+	private String bikeId;
+	private String bikeBrand;
+	private String bikeModel;	
+	private double bikeBasePricePerDay;
+	private boolean bikeisAvailable;
+
+	Bike(String bikeId, String bikeBrand, String bikeModel, double bikeBasePricePerDay) {
+		this.bikeId = bikeId;
+		this.bikeBrand = bikeBrand;
+		this.bikeModel = bikeModel;
+		this.bikeBasePricePerDay = bikeBasePricePerDay;
+		this.bikeisAvailable = true;
+	}
+
+	public String getBikeId() {
+		return bikeId;
+	}
+
+	public String getBikeBrand() {
+		return bikeBrand;
+	}
+
+	public String getBikeModel() {
+		return bikeModel;
+	}
+
+	public double calculatePrice1(int rentalDays1) {
+		return bikeBasePricePerDay * rentalDays1;
+	}
+
+	public boolean bikeisAvailable() {
+		return bikeisAvailable;
+	}
+
+	public void rent() {
+		bikeisAvailable = false;
+	}
+
+	public void returnBike(){	
+		bikeisAvailable = true;
+	}
+}
